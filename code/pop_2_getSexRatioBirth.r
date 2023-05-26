01a2d4c0cdf3f6a28a56edb5bc0d92e3d1e9c1e2
Kiesha Prem <kiesha.prem@gmail.com>
2023-05-26 15:48:44 +0800

    updates

--- code/pop_2_getSexRatioBirth.r
+++
@@ -1,7 +0,0 @@
-popsrb = read.csv('data/population/pop2100_sbr.csv',as.is = TRUE)
-popsrb$iso3c = countrycode(popsrb$country,origin = 'country.name',destination = 'iso3c')
-popsrb$iso3c[is.na(popsrb$iso3c)] = 'CHI'
-popsrb = popsrb[order(popsrb$iso3c),]
-
-# first, work out the approximate sex ratio at birth
-popsrb$srb_male =  popsrb$males0to4/(popsrb$males0to4+popsrb$females0to4)
